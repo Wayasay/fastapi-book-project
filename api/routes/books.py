08a8ecfc570882d4a8ca69b2518b008deec96371
orobosa <corneliusorobosa22@gmail.com>
2025-02-11 17:41:17 +0000

    fixed pytest

--- api/routes/books.py
+++ api/routes/books.py
@@ -48,14 +48,16 @@ async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
 
 @router.get(
-    "/{book_id}", status_code=status.HTTP_200_OK
+    "/{book_id}",response_model=Book, status_code=status.HTTP_200_OK
 )
 async def get_book(book_id: int) ->  Book:
-    # selected_book = db.get_book(book_id)
-    selected_book = False
+    selected_book = db.get_book(book_id)
     if not selected_book:
-        return JSONResponse(status_code=404, content= None, detail="Book not found")
-    return selected_book
+        return JSONResponse(status_code=404, content= None,)
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content=selected_book.model_dump(),
+    )
 
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def update_book(book_id: int, book: Book) -> Book:
